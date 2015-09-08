c06bb52543152478390fc24a7527b8ba2fc20980
Suchan Lee <suchanl@palantir.com>
2015-09-08 00:39:06 -0700

    asdf

--- static/js/main.js
+++ static/js/main.js
@@ -20,60 +20,79 @@
   // List of projects
   var projects = [
     {
-      title: 'Rethinking CaltrainMe',
-      thumbnail: '/static/img/projects/caltrainme/thumbnail_caltrain.png',
-      mainImage: '/static/img/projects/caltrainme/iphone-6-mockup.png',
-      projectType: 'Conceptual Redesign',
-      medium: 'Mobile App',
-      year: '2014',
-      projectDetail: Handlebars.templates.caltrainMe()
+      title         : 'Rethinking CaltrainMe',
+      thumbnail     : '/static/img/projects/caltrainme/thumbnail_caltrain.png',
+      mainImage     : '/static/img/projects/caltrainme/iphone-6-mockup.png',
+      projectType   : 'Conceptual Redesign',
+      medium        : 'Mobile App',
+      year          : '2014',
+      projectDetail : Handlebars.templates.caltrainMe()
     },
     {
-      title: 'Chrome Download Redesign',
-      slug: 'chrome-download',
-      thumbnail: '/static/img/projects/chromedl/thumbnail-07.png',
-      mainImage: '/static/img/projects/chromedl/chrome.gif',
-      projectType: 'Conceptual Redesign',
-      medium: 'Browser',
-      year: '2015',
-      projectDetail: Handlebars.templates.chromedownload()
+      title         : 'Chrome Download Redesign',
+      slug          : 'chrome-download',
+      thumbnail     : '/static/img/projects/chromedl/thumbnail-07.png',
+      mainImage     : '/static/img/projects/chromedl/chrome.gif',
+      projectType   : 'Conceptual Redesign',
+      medium        : 'Browser',
+      year          : '2015',
+      projectDetail : Handlebars.templates.chromedownload()
     },
     {
-      title: 'Yogo: Your Poll on the Go',
-      slug: 'yogo',
-      mainImage: '/static/img/projects/yogo/logo01.png',
-      thumbnail: '/static/img/projects/yogo/thumbnail_yogo.png',
-      projectType: 'Product Design',
-      medium: 'Web App',
-      year: '2015',
-      summary: 'Lorem ipsum In in officia culpa et elit in dolore consequat deserunt elit dolor dolor culpa deserunt eu Ut mollit.',
-      projectDetail: Handlebars.templates.yogo()
+      title         : 'Yogo: Your Poll on the Go',
+      slug          : 'yogo',
+      mainImage     : '/static/img/projects/yogo/logo01.png',
+      thumbnail     : '/static/img/projects/yogo/thumbnail_yogo.png',
+      projectType   : 'Product Design',
+      medium        : 'Web App',
+      year          : '2015',
+      projectDetail : Handlebars.templates.yogo()
     },
     {
-      title: 'Gas Buddy Redesign',
-      mainImage: '/static/img/projects/gasbuddy/gasbuddy_sp.png',
-      thumbnail: '/static/img/projects/gasbuddy/thumbnail_gasbuddy.png',
-      projectType: 'Conceptual Redesign',
-      medium: 'Mobile App',
-      year: '2014',
-      projectDetail: Handlebars.templates.gasbuddy()
+      title         : 'Gas Buddy Redesign',
+      mainImage     : '/static/img/projects/gasbuddy/gasbuddy_sp.png',
+      thumbnail     : '/static/img/projects/gasbuddy/thumbnail_gasbuddy.png',
+      projectType   : 'Conceptual Redesign',
+      medium        : 'Mobile App',
+      year          : '2014',
+      projectDetail : Handlebars.templates.gasbuddy()
     },
     {
-      title: 'Icons',
-      thumbnail: '/static/img/projects/icons/thumbnail_beauty02.png',
-      projectType: 'Icons',
-      medium: 'Illustration',
-      year: '2015',
-      projectDetail: Handlebars.templates.icons()
+      title         : 'Icons',
+      thumbnail     : '/static/img/projects/icons/thumbnail_beauty02.png',
+      projectType   : 'Icons',
+      medium        : 'Illustration',
+      year          : '2015',
+      projectDetail : Handlebars.templates.icons()
     },
     {
-      title: 'Japan Trip Icon Alphabet Book',
-      thumbnail: '/static/img/projects/japanalphabets/thumbnail_abc.png',
-      projectType: 'Icons',
-      medium: 'Illustration',
-      year: '2015',
-      projectDetail: Handlebars.templates.japanIcons()
+      title         : 'Japan Trip Icon Alphabet Book',
+      slug          : 'japan-trip-icons',
+      thumbnail     : '/static/img/projects/japanalphabets/thumbnail_abc.png',
+      projectType   : 'Icons',
+      medium        : 'Illustration',
+      year          : '2015',
+      projectDetail : Handlebars.templates.japanIcons()
+    },
+    {
+      title         : 'Just Show Up',
+      slug          : 'just-show-up',
+      thumbnail     : '/static/img/projects/justshowup/thumbnail-08.png',
+      projectType   : 'Event Promotion',
+      medium        : 'Poster',
+      year          : '2015',
+      projectDetail : Handlebars.templates.justshowup()
+    },
+    {
+      title         : 'The Exhibition',
+      slug          : 'the-exhibition',
+      thumbnail     : '/static/img/projects/theexhibition/thumbnail-09.png',
+      projectType   : 'Product',
+      medium        : 'Web',
+      year          : '2014',
+      projectDetail : Handlebars.templates.japanIcons()
     }
+
   ];
 
   var getPath = function(id) {
@@ -122,36 +141,58 @@
     evt.stopPropagation();
   });
 
-  $projectList.on('click', '.project-thumbnail', handleDetailViewClick);
+  $('.projects-link').click(function(evt) {
+    if (window.location.pathname !== '/') {
+      router.setRoute('/');
+    }
+    evt.preventDefault();
+    evt.stopPropagation();
+  });
+
+  $('.about-link').click(function(evt) {
+    if (window.location.pathname !== '/about') {
+      router.setRoute('/about');
+    }
+    evt.preventDefault();
+    evt.stopPropagation();
+  });
+
+  $projectList.on('click', '.project-thumbnail',    handleDetailViewClick);
   $projectList.on('click', '.project-item-heading', handleDetailViewClick);
-  $projectList.on('click', '.project-item-title', handleDetailViewClick);
+  $projectList.on('click', '.project-item-title',   handleDetailViewClick);
 
   var hideNonProjectListViews = function() {
     $projectDetail.hide();
-    $contact.hide();
     $about.hide();
   };
 
   // views
   var landingView = function() {
     hideNonProjectListViews();
+    $projectList.show();
     $projectDetail.html('').hide();
     $('.project-item').css('opacity', 0).each(function(idx) {
       $(this).delay((i++) * 80).fadeTo(250, 1);
     });
     $body.animate({ scrollTop: 0 });
+    document.title = 'Amy J Lee';
   };
 
   var detailView = function(projectId) {
     var project = projects[projectId];
     $projectDetail.html(Handlebars.templates.projectDetail(project));
     $projectDetail.show();
+    $projectList.show();
+    $about.hide();
+    document.title = project.title;
   };
 
   var aboutView = function() {
     hideNonProjectListViews();
     $about.show();
+    $projectList.hide();
     $body.animate({ scrollTop: 0 });
+    document.title = 'About Amy';
   };
 
   var router = Router({
--- static/js/templates/projects/japanIcons.handlebars.js
+++ static/js/templates/projects/japanIcons.handlebars.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['japanIcons'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<img src=\"/static/img/projects/japanalphabets/japan_iconset-01.png\" alt=\"Japan Icon Set\" class=\"project-image image-width-100\">\n";
+    return "<p class=\"japan-trip-icons-text\">Alphabet book of things that I saw, drank, ate, and experienced during my Japan trip.</p>\n<img src=\"/static/img/projects/japanalphabets/japan_iconset-01.png\" alt=\"Japan Icon Set\" class=\"project-image image-width-100\">\n";
 },"useData":true});
 })();
