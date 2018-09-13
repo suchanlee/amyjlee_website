2eea7bfc4bf5a32b28b8104c7066b61e6bcbc005
Suchan Lee <suchanl@palantir.com>
2018-09-13 00:38:09 -0400

    update case study structures

--- static/js/templates/projectDetail.handlebars.js
+++ static/js/templates/projectDetail.handlebars.js
@@ -7,7 +7,7 @@ templates['projectDetail'] = template({"1":function(container,depth0,helpers,par
     + alias4(((helper = (helper = helpers.mainImage || (depth0 != null ? depth0.mainImage : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"mainImage","hash":{},"data":data}) : helper)))
     + "\" alt=\""
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + " main image\" class=\"project-main-image project-image image-width-60\">\n";
+    + " main image\" class=\"project-main-image\">\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
@@ -18,9 +18,7 @@ templates['projectDetail'] = template({"1":function(container,depth0,helpers,par
     + "  <div class=\"project-detail-header\">\n    <h1 class=\"project-heading\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h1>\n    <p class=\"project-metadata\">\n      <span class=\"project-metadata-type\">"
-    + alias4(((helper = (helper = helpers.projectType || (depth0 != null ? depth0.projectType : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"projectType","hash":{},"data":data}) : helper)))
-    + "</span> |\n      <span class=\"project-metadata-year\">"
-    + alias4(((helper = (helper = helpers.year || (depth0 != null ? depth0.year : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"year","hash":{},"data":data}) : helper)))
+    + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + "</span>\n    </p>\n  </div>\n  <div class=\"clear-float\"></div>\n  <div class=\"project-details\">\n    "
     + ((stack1 = ((helper = (helper = helpers.projectDetail || (depth0 != null ? depth0.projectDetail : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"projectDetail","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "\n  </div>\n</div>\n";
